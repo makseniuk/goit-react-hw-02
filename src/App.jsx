efad6f95eb277ff2638fea21b2fdd1624846e4c9
makseniuk <144346873+makseniuk@users.noreply.github.com>
2024-04-05 14:30:55 +0200

    test-1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import './App.css';
+import { useEffect, useState } from 'react';
 import Description from './components/Description';
 import Options from './components/Options'
 import Notification from './components/Notification ';
 import Feedback from './components/FeedBack';
+import './App.css';
 
 function App() {
   const [feedbackCounts, setFeedbackCounts] = useState({
@@ -12,25 +12,58 @@ function App() {
     bad: 0
   });
 
-  const updateFeedback = (feedbackCount) => {
-    setFeedbackCounts(prevState => ({
-      ...prevState,
-      [feedbackCount]: prevState[feedbackCount] + 1
-    }));
+useEffect(() => {
+    const savedCounts = JSON.parse(window.localStorage.getItem("savedFeedbackCounts"));
+    if (savedCounts) {
+      setFeedbackCounts(savedCounts);
+    }
+  }, []);
+
+useEffect(() => {
+  window.localStorage.setItem("savedFeedbackCounts", JSON.stringify(feedbackCounts));
+}, [feedbackCounts]);
+
+useEffect(() => {
+  const handleBeforeUnload = () => {
+    window.localStorage.setItem("savedFeedbackCounts", JSON.stringify(feedbackCounts));
   };
+  window.addEventListener("beforeunload", handleBeforeUnload);
+  return () => {
+    window.removeEventListener("beforeunload", handleBeforeUnload);
+  };
+}, [feedbackCounts]);
 
-    const totalFeedback = feedbackCounts.good + feedbackCounts.neutral + feedbackCounts.bad;
+const updateFeedback = feedbackCount => {
+  setFeedbackCounts(prevState => ({
+    ...prevState,
+    [feedbackCount]: prevState[feedbackCount] + 1
+  }));
+};
 
+const resetFeedback = () => {
+  setFeedbackCounts({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+};
 
-  return (
-    <div className="App">
-      <Description />
-      <Feedback updateFeedback={updateFeedback} />
-      <Options feedbackCounts={feedbackCounts} />
-      {totalFeedback > 0 ? <Feedback feedbackTypes={feedbackCounts} totalFeedback={totalFeedback} /> : <Notification />}
-
-    </div>
-  );
+const totalFeedback = feedbackCounts.good + feedbackCounts.neutral + feedbackCounts.bad;
+const positivePercentage = totalFeedback > 0 ? Math.round((feedbackCounts.good / totalFeedback) * 100) : 0;
+return (
+ <div className="App">
+   <Description />
+   <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} resetFeedback={resetFeedback} />
+   {totalFeedback === 0 ? <Notification /> : null}
+   {totalFeedback > 0 ? <Feedback feedbackCounts={feedbackCounts} totalFeedback={totalFeedback} /> : null}
+    {totalFeedback > 0 && (
+     <>
+       <p>Positive Feedback Percentage: {positivePercentage}%</p>
+     </>
+   )}
+ </div>
+ );
 }
+
 
 export default App;
--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,7 +1,7 @@
 const Description = () => {
     return (
         <>
-            <h1>Bienvenue au Café Sip Happens</h1>
+            <h1>Sip Happens Café</h1>
             <p>
                 Please leave your feedback about ourservice by selecting one of the options below.
             </p>
